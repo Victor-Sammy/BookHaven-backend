270007959e7963003275a89fcca8615b9dfba6d1
Victor-Sammy <victorsammy1997@gmail.com>
2023-10-01 19:18:02 +0100

    updated passport.js file

--- passport.js
+++ passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: `${process.env.CLIENT_ID}`,
       clientSecret: `${process.env.CLIENT_SECRET}`,
-      callbackURL: `${process.env.GOOGLE_CALLBACK_URL}`,
+      callbackURL: '/auth/google/callback',
       scope: ['profile', 'email'],
     },
     async (_accessToken, _refreshToken, profile, done) => {
